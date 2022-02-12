39331d3786ed549d0b8091e5fb42109113d08ae0
lathoub <4082369+lathoub@users.noreply.github.com>
2022-02-12 20:25:06 +0100

    re-order structure

---
+++ 2_LogWindow/2_LogWindow.ino
@@ -0,0 +1,18 @@
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {};
+  delay(1000);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.println("setup");
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  Serial.println("aan");
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+  Serial.println("uit");
+}

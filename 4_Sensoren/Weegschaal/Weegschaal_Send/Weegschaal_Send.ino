a0d42d333b85dab3e2f8e8e1ee24ae31b753178d
lathoub <4082369+lathoub@users.noreply.github.com>
2022-02-12 14:39:00 +0100

    added Weegschaal

---
+++ 4_Sensoren/Weegschaal/Weegschaal_Send/Weegschaal_Send.ino
@@ -0,0 +1,26 @@
+#include "HX711.h" // https://github.com/RobTillaart/HX711
+
+HX711 scale; // instance 
+
+const uint8_t dataPin = D2;
+const uint8_t clockPin = D3;
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial) {};
+
+  scale.begin(dataPin, clockPin);
+
+  // Scale resolution max 5kg
+  scale.set_scale(476); // manually calibrated
+ 
+  scale.tare(); // reset the scale to zero = 0
+}
+
+void loop()
+{
+  auto f = scale.get_units(5);
+  Serial.println(f);
+  delay(250); // continuously measure, 4x per second
+}

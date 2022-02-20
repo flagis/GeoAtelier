238c9cb3982385b8432163e59680d3eab6d04e8a
lathoub <4082369+lathoub@users.noreply.github.com>
2022-02-20 22:31:15 +0100

    move image

--- 7.1_SensorThingsAPI_GET/settings.h
+++ 7.1_SensorThingsAPI_GET/settings.h
@@ -4,5 +4,6 @@ const char protocol[] = "http";
 const char host[] = "192.168.0.105";
 const uint16_t port = 49154;
 const char path[] = "/v1.0";
+
 auto domain = String(host) + ":" + port + "/FROST-Server";
 auto base_url = String(protocol) + "://" + domain + path;

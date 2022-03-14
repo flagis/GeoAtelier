61d6adc3b353b036be371f8184cf3fa4dcaf1613
lathoub <4082369+lathoub@users.noreply.github.com>
2022-03-14 09:35:57 +0100

    Update credentials.h

--- 7.1_SensorThingsAPI_GET/credentials.h
+++ 7.1_SensorThingsAPI_GET/credentials.h
@@ -2,6 +2,3 @@
 
 char ssid[] = "FLAGIS";     // ssid
 char pass[] = "flagis2022"; // wifi password
-
-char user[]     = "flagis";      // MySQL user login username
-char password[] = "Flagis2022_"; // MySQL user login password

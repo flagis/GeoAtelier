f580a80b43cf270f8ca121cd93e149c924d07d01
lathoub <4082369+lathoub@users.noreply.github.com>
2022-03-26 19:08:14 +0100

    Update stapi_settings.h

--- 7_SensorThingsAPI/utils/stapi_settings.h
+++ 7_SensorThingsAPI/utils/stapi_settings.h
@@ -1,8 +1,8 @@
 #pragma once
 
 const char protocol[] = "http";
-const char host[] = "192.168.0.105";
-const uint16_t port = 8080;
+const char host[] = "192.168.1.10";
+const uint16_t port = 80;
 const char path[] = "/v1.1";
 
 auto domain = String(host) + ":" + port + "/FROST-Server";

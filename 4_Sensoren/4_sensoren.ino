9add304ca392212e34870441fe37c41a2e5cadcc
lathoub <4082369+lathoub@users.noreply.github.com>
2022-05-01 09:58:48 +0200

    update fritzing, added pngs

--- 4_Sensoren/4_sensoren.ino
+++ 4_Sensoren/4_sensoren.ino
@@ -1,17 +1,17 @@
 #include "utils/logging.h"
 
-//#include "1234.h"
+// Uncomment the line below, corresponding with the used sensors
 //#include "ADXL345.h"
+//#include "AGS02MA.h"
+//#include "AM2320.h"
 //#include "BMP085.h"
 //#include "BMP180.h"
-//#include "buttons.h"
+//#include "HCSR04.h"
 //#include "HMC5883.h"
 //#include "HX711.h"
-//#include "HCSR04.h"
-//#include "MB060.h"
+//#include "keyboard.h"
 //#include "MAX4466.h"
-//#include "AM2320.h"
-//#include "AGS02MA.h"
+//#include "MB060.h"
 
 void setup() {
   setupLogging();
--- 6_SendReading/6_SendReading.ino
+++ 6_SendReading/6_SendReading.ino
@@ -11,18 +11,18 @@
 
 void transmitValue(float value);
 
-//#include "1234.h"
+// Uncomment the line below, corresponding with the used sensors
 //#include "ADXL345.h"
+//#include "AGS02MA.h"
+//#include "AM2320.h"
 //#include "BMP085.h"
 //#include "BMP180.h"
-//#include "buttons.h"
+//#include "HCSR04.h"
 //#include "HMC5883.h"
 //#include "HX711.h"
-//#include "HCSR04.h"
-//#include "MB060.h"
+//#include "keyboard.h"
 //#include "MAX4466.h"
-//#include "AM2320.h"
-#include "AGS02MA.h"
+//#include "MB060.h"
 
 MySQL_Connection conn((Client *)&client);
 auto query_mem = MySQL_Query(&conn);
